5b3cbef9d8bede9e0c5165a0aefef0eef62f985a
Joseph98765 <80423648+Joseph98765@users.noreply.github.com>
2021-03-10 17:03:38 -0500

    Partie 1
    Tâche switch LED

--- codegentemp/Labo4_u.sdc
+++ codegentemp/Labo4_u.sdc
@@ -1,3 +1,3 @@
 # Component constraints for C:\Users\jlabe\Desktop\Ecole\Polytechnique\2020-2021\H21\GBM2100_Projet\Labo4\Labo4.cydsn\TopDesign\TopDesign.cysch
 # Project: C:\Users\jlabe\Desktop\Ecole\Polytechnique\2020-2021\H21\GBM2100_Projet\Labo4\Labo4.cydsn\Labo4.cyprj
-# Date: Wed, 10 Mar 2021 20:31:30 GMT
+# Date: Wed, 10 Mar 2021 21:12:08 GMT
--- main_cm4.c
+++ main_cm4.c
@@ -16,11 +16,23 @@
 #include "params.h"
 #include "queue.h"
 
+void vSwitchLEDTask()
+{
+    for (;;)
+    {
+        vTaskDelay(pdMS_TO_TICKS(500));
+        Cy_GPIO_Write(LED_0_PORT, LED_0_NUM,!Cy_GPIO_Read(LED_0_PORT, LED_0_NUM));
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
 int main(void)
 {
     __enable_irq(); /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    xTaskCreate(vSwitchLEDTask, "LED", 400, NULL, 1, NULL);
+    vTaskStartScheduler();
 
     for(;;)
     {
